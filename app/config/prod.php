128f751e485bd7a59cb0b845de3b4fca7a9541d9
Cyril <cyril.sadovnik@gmail.com>
2016-10-10 10:48:12 +0300

    Added dotenv

--- app/config/prod.php
+++ app/config/prod.php
@@ -2,7 +2,22 @@
 
 use Saxulum\DoctrineMongoDb\Provider\DoctrineMongoDbProvider;
 
+$dotenv = new Dotenv\Dotenv(dirname(__DIR__));
+$dotenv->load();
+
 /** @var Silex\Application $app */
+$app['mongodb.default_db'] = getenv('MONGO_DB');
 $app->register(new DoctrineMongoDbProvider(), [
-    'mongodb.options' => [ 'server' => 'mongodb://localhost:27017' ]
+    'mongodb.options' => [
+        'server' => getenv('MONGO_SERVER'),
+        'options' => [
+            'username' => getenv('MONGO_USER'),
+            'password' => getenv('MONGO_PASSWORD'),
+            'db' => getenv('MONGO_DB')
+        ]
+    ]
 ]);
+
+$app['repository.car'] = function ($app) {
+    return new CarMongoRepository($app['mongodb'], $app['mongodb.default_db']);
+};
